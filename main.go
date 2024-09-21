792fff9ba501f23ae69739da96eb1f66aa8f9480
KagChi <yonuxbot@gmail.com>
2024-09-21 21:51:04 +0700

    feat: error handling

--- main.go
+++ main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"errors"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"orchestrator/router"
+	"orchestrator/struct_type"
 )
 
 func main() {
 	app := fiber.New(fiber.Config{
 		Prefork:      true,
 		ServerHeader: "Fiber",
+		ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+			// Status code defaults to 500
+			code := fiber.StatusInternalServerError
+
+			// Retrieve the custom status code if it's a *fiber.Error
+			var e *fiber.Error
+			if errors.As(err, &e) {
+				code = e.Code
+			}
+
+			return ctx.Status(code).JSON(&struct_type.ErrorResponse{
+				Message: "Something went wrong, please try again later.",
+				Error:   err.Error(),
+			})
+		},
 	})
+
+	app.Use(recover.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Arjuna Orchestrator 🔥")
